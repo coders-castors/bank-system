2506b9891bd4f27872fb44fa0af44286b1f39ce2
Caleb Clark <153481423+coders-castors@users.noreply.github.com>
2023-12-11 12:37:44 +0700

    Create account.py

---
+++ account.py
@@ -0,0 +1,18 @@
+# account.py
+
+class Account:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient funds")
+
+    def get_balance(self):
+        return self.balance
